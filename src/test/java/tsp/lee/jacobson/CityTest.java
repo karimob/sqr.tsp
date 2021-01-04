a63b41c171ec8d77473bc7953e9b964d5237ca76
narcisse <narcisse@DESKTOP-43Q0T61>
2021-01-04 14:37:22 +0100

    Premier test de distanceto(City) dans CityTest

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -6,9 +6,23 @@ import org.junit.Test;
 
 public class CityTest {
 
+	
+	/*GIVEN City=DIJON
+	 * WHEN X=5, y=1
+	 * THEN DISTANCE=13
+	 */
+	City REF= new City(17,6);
+	//City REF= new City();
+	City Dijon =new City (5,1);
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void distanceTo_Dijon_13() {
+		int xDistance = Math.abs(REF.getX() - Dijon.getX());
+        int yDistance = Math.abs(REF.getY() - Dijon.getY());
+        double expected = 13;
+        
+        double actual = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance) );
+        assertEquals(expected,actual,8);
+		
 	}
 
 }
